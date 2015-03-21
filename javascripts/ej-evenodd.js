5e4079695121eba024ea1bd293fb93ef4ae2ea63
mcmillenb <brianmcmillen1@gmail.com>
2015-03-21 05:38:03 +0000

    elequent javascript isEven exercise

---
+++ javascripts/ej-evenodd.js
@@ -0,0 +1,16 @@
+function isEven(num) {
+    if (num < 0) num *= -1;
+    if (num == 1) {
+        return false;
+    }
+    else if (num == 0){
+        return true;
+    }
+    else{
+        return isEven(num-2);
+    }
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
