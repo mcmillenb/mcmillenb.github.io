677207fa462d2cf8dd5325922ed54148d21acdd6
mcmillenb <brianmcmillen1@gmail.com>
2015-03-21 05:45:24 +0000

    brevity

--- javascripts/ej-evenodd.js
+++ javascripts/ej-evenodd.js
@@ -2,15 +2,9 @@ function isEven(num) {
     // recursive function to return true if a given number is even and
     // false if it is odd. 
     if (num < 0) num *= -1; // if the number is negative, make it positive
-    if (num == 1) {
-        return false;
-    }
-    else if (num == 0){
-        return true;
-    }
-    else{
-        return isEven(num-2);
-    }
+    if (num == 1) return false;
+    if (num == 0) return true;
+    return isEven(num-2);
 }
 
 console.log(isEven(50));
