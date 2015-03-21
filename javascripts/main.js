92090ff3ba7cece2e15f51087276524a25054a06
mcmillenb <brianmcmillen1@gmail.com>
2015-03-21 05:31:14 +0000

    elequent javascript exercise

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+function zeroPad(number, width){
+    var string = String(number);
+    while (string.length < width)
+        string = "0" + string;
+    return string;
+}
+
+function printFarmInventory(cows, chickens, pigs) {
+    console.log(zeroPad(cows, 3) + " Cows");
+    console.log(zeroPad(chickens, 3) + " Chickens");
+    console.log(zeroPad(pigs, 3) + " Pigs");
+}
+
+printFarmInventory(7, 16, 3);
