b6bf7c820d0ba752b32b6ad4b69e9e89a651c988
mcmillenb <brianmcmillen1@gmail.com>
2015-03-22 19:30:11 +0000

    .some and .every mock array functions for Elequent Javascript exercise

---
+++ javascripts/eloquentjs/Ch 5 - Higher-Order Functions/someAndEveryEx.js
@@ -0,0 +1,28 @@
+function every(array, test){
+  // returns true if every element in an array meets
+  // some test (function)
+  for (var i = 0; i < array.length; i++){
+    if (!test(array[i])) return false; 
+  }
+  
+  return true;
+}
+
+function some(array, test){
+  // returns true if any element in an array meets
+  // some test (function)
+  for (var i = 0; i < array.length; i++){
+    if (test(array[i])) return true;
+  }
+  
+  return false;
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
