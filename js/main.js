4e325ae6209d5c3738c6e6b353ef514f78557ed5
Brian McMillen <brian@offcampuspartners.com>
2016-11-08 12:44:02 -0500

    Convert tabs to spaces

--- js/main.js
+++ js/main.js
@@ -8,18 +8,18 @@ var dots = [];
 
 // Add a new MusicDot when user clicks the sandbox
 $('body').on('mousedown touchstart', '#sandbox', (e) => {
-	if (e.shiftKey) {
-		return;
-	}
-	var type = $soundType.val();
-	dots.push(new MusicDot(e, type));
+    if (e.shiftKey) {
+        return;
+    }
+    var type = $soundType.val();
+    dots.push(new MusicDot(e, type));
 });
 
 // Remove all of the MusicDots when user clicks Reset
 $('body').on('click', '#reset', (e) => {
-	for (var i = 0; i < dots.length; i++) {
-		dots[i].remove();
-		delete dots[i];
-	}
-	dots = [];
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].remove();
+        delete dots[i];
+    }
+    dots = [];
 });
