612e9eb048f1d79fef38fd9815bf1fa047398546
Brian <brianmcmillen1@gmail.com>
2016-11-13 22:25:34 -0500

    Finnish drag and drop functionaly

--- js/launch_screen/main.js
+++ js/launch_screen/main.js
@@ -1,17 +1,37 @@
-var pressing = false;
-var dragging = false;
-var $icons   = $('.icon');
-var $page    = $('#page');
-$page.sortable({delay: 500});
+// whether or not the mouse is down on an icon
+var _pressing  = false;
+// whether or not an icon is in the process of being moved
+var _dragging  = false;
+// timeout to delay the drag effect
+var _pressTimeout;
+
+// the app icon elements
+var $icons = $('.icon');
+// the element containing the app icons
+var $page  = $('#page');
+
+// make the app icons sortable
+$page.sortable({ 
+  containment: 'parent',
+  stop: function(e, ui) { 
+    _pressing = false;
+    $(this).sortable('disable');
+  }
+}).sortable('disable'); // disable initially
 $page.disableSelection();
 
+/**
+ * Animate an icon element to wiggle rotationally
+ * @param  {object} $icon App icon element to apply animation to
+ * @return {void}
+ */
 function wiggleIcon($icon) {
-  $({deg: 0}).animate({deg: 16}, {
+  var $dummy = $({ deg: 0 }); // dummy object with 'deg' property 
+  $dummy.animate({ deg: 16 }, { // animate 16 steps
     duration: 1200,
-    step: function(now) {
-      var deg;
-
-      if (!dragging) { // when not dragging
+    step: function(now) { 
+      var deg; // the degree of rotation to animate the icon to
+      if (!_dragging) { // when not dragging
         deg = 0; // set rotation to 0
       } else if (now > 12) { // at time 12
         deg = now - 16; // go from -4deg to 0deg
@@ -32,17 +52,21 @@ function wiggleIcon($icon) {
   });
 };
 
+/**
+ * Initiate the icon dragging effect
+ * @return {void} 
+ */
 function startDragging() {
-  if (!pressing) {
+  if (!_pressing) { // dragging can only happen when pressing
     return;
   }
-  dragging = true;
-  $icons.each(function() {
+  _dragging = true;
+  $icons.each(function() { // wiggle all of the icons when dragging one
     var $icon = $(this);               
     wiggleIcon($icon);
     var interval = window.setInterval(function() {
-      if (dragging) {
-        wiggleIcon($icon);
+      if (_dragging) { 
+        wiggleIcon($icon); 
       } else {
         clearInterval(interval);
       }
@@ -50,24 +74,28 @@ function startDragging() {
   });
 };
 
-function stopDragging() {
-  pressing = false;
-  dragging = false;
-};
+/** EVENT LISTENERS **/
 
-var $body = $('body');
-$icons.mousedown(function() {
-  $icon = $(this);
-  pressing = true;
-  setTimeout(function() {
+$icons.mousedown(function(e) { // when the mouse goes down on an icon
+  if (_dragging) {
+    return;
+  }
+  var $this = $(this);
+  var event = e;
+  _pressing = true;
+  _pressTimeout = setTimeout(function() { // start dragging after half a second
+    $page.sortable('enable'); // enable the sort (and drag) effects on icons
     startDragging();
-  }, 500);
+    $this.trigger(event);
+  }, 500); 
+}).mousemove(function() { // when the mouse moves on an icon
+  clearTimeout(_pressTimeout);
+  _pressing = false; // then the state is set to not pressing
 });
 
-$icons.mouseup(stopDragging);
 
-$icons.mouseleave(function() {
-  if (!dragging) {
-    pressing = false;
-  }
+$('body').mouseup(function() { // when the mouse goes up anywhere in the body
+  clearTimeout(_pressTimeout);
+  _pressing = false; // the state is set to not pressing
+  _dragging = false; // and not dragging
 });
